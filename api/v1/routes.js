79ddd5d699c06e90dca4adc18185e66578a70d63
mohhd-arif <arifali123454150@gmail.com>
2019-11-05 21:41:03 +0530

    MVC model initialized

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const route = express.Router();
 const bodyparser = require('body-parser');
+// route.set("view engine","html");
 
 
 // route.use(bodyparser.json());
 route.get('/hey',(req,res)=>{
   console.log("hello");
   res.send('hey').status(200);
+});
+
+route.get('/map',(req,res)=>{
+  res.render('index.ejs');
 });
 
 module.exports = route;
--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyparser = require('body-parser')
 const port = process.env.PORT || 8000;
 const routes = require('./api/v1/routes')
+const path = require('path')
+
+// app.set('view engine','ejs');
+// app.set('views',path.join(__dirname, 'views'));
 
 // app.use(bodyparser);
 app.use('/api/v1',routes);
